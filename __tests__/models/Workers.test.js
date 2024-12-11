337961d63a7cde26877e45fad119cb20f2015230
suhwa <tnghk9611@naver.com>
2024-12-11 15:36:15 +0900

    test: 근무자가 겹치는 경우 테스트 작성

--- __tests__/models/Workers.test.js
+++ __tests__/models/Workers.test.js
@@ -6,7 +6,16 @@ describe("Workers 클래스 테스트 작성", () => {
     const workers = new Workers(workersInput);
 
     workersInput.forEach((worker) => {
-      expect(workers.getNextWorker()).toEqual(worker);
+      expect(workers.assignWorker()).toEqual(worker);
     });
+  });
+
+  test("이전 근무자와 순번이 겹치면 다음 근무자와 자리를 바꾸어야 한다", () => {
+    const workersInput = ["루루", "솔로스타", "수아", "슬링키", " 참새"];
+    const workers = new Workers(workersInput);
+
+    expect(workers.assignWorker()).toEqual(workersInput[0]); // 루루 할당
+    expect(workers.changeOrder(workersInput[1])).toEqual(workersInput[2]); // 자리를 바꿨으니 솔로스타가 아니라 수아를 배정
+    expect(workers.assignWorker()).toEqual(workersInput[1]); // 다시 원래대로 솔로스타
   });
 });
