ac8566fef7c43117ef4cc8d9153dc4749b9dad2c
suhwa <tnghk9611@naver.com>
2024-12-11 15:08:48 +0900

    feat: 닉네임 길이 제한 유효성 검사 구현

--- src/validators/WorkerValidator.js
+++ src/validators/WorkerValidator.js
@@ -3,6 +3,7 @@ import { generateError } from "../utils/generateError.js";
 class WorkerValidator {
   static validateWorker(workers) {
     this.#validateDuplicate(workers);
+    this.#validateNameLength(workers);
   }
 
   static #validateDuplicate(workers) {
@@ -10,6 +11,13 @@ class WorkerValidator {
 
     if (set.size !== workers.length) {
       generateError("중복된 닉네임이 있습니다.");
+    }
+  }
+
+  static #validateNameLength(workers) {
+    console.log(workers);
+    if (workers.some((name) => name.length > 5)) {
+      generateError("닉네임은 최대 5자까지 작성할 수 있습니다.");
     }
   }
 }
