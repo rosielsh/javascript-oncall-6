f34d72fd3b8395e4027eb33a097c506237bd625d
suhwa <tnghk9611@naver.com>
2024-12-11 15:13:53 +0900

    feat: 근무자 명 수 예외처리 구현

--- src/validators/WorkerValidator.js
+++ src/validators/WorkerValidator.js
@@ -4,6 +4,7 @@ class WorkerValidator {
   static validateWorker(workers) {
     this.#validateDuplicate(workers);
     this.#validateNameLength(workers);
+    this.#validateTotalCount(workers);
   }
 
   static #validateDuplicate(workers) {
@@ -15,9 +16,14 @@ class WorkerValidator {
   }
 
   static #validateNameLength(workers) {
-    console.log(workers);
     if (workers.some((name) => name.length > 5)) {
       generateError("닉네임은 최대 5자까지 작성할 수 있습니다.");
+    }
+  }
+
+  static #validateTotalCount(workers) {
+    if (workers.length < 5 || workers.length > 35) {
+      generateError("비상 근무자는 최소 5명 ~ 최대 35명 등록할 수 있습니다.");
     }
   }
 }
