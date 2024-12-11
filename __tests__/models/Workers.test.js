7dbe141408bf8d442f08f84751849730285f5540
suhwa <tnghk9611@naver.com>
2024-12-11 15:29:22 +0900

    feat: 다음 근무할 순번을 알아오는 기능 구현

--- __tests__/models/Workers.test.js
+++ __tests__/models/Workers.test.js
@@ -1,4 +1,6 @@
-descibe("Workers 클래스 테스트 작성", () => {
+import Workers from "../../src/models/Workers.js";
+
+describe("Workers 클래스 테스트 작성", () => {
   test("다음 근무할 순번의 근무자를 알 수 있다", () => {
     const workersInput = ["루루", "솔로스타", "수아", "슬링키", " 참새"];
     const workers = new Workers(workersInput);
