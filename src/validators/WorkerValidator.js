3fcf805c0db7df9dc4a34ebc3ff8fe6125cd7d93
suhwa <tnghk9611@naver.com>
2024-12-11 15:20:11 +0900

    feat: 근무자를 평일, 휴일에 한 번씩 배정하지 않는 예외 처리 구현

--- src/validators/WorkerValidator.js
+++ src/validators/WorkerValidator.js
@@ -7,6 +7,10 @@ class WorkerValidator {
     this.#validateTotalCount(workers);
   }
 
+  static validateWorkers(weekDayWorkers, holidayWorkers) {
+    this.#validateSame(weekDayWorkers, holidayWorkers);
+  }
+
   static #validateDuplicate(workers) {
     const set = new Set(workers);
 
@@ -24,6 +28,17 @@ class WorkerValidator {
   static #validateTotalCount(workers) {
     if (workers.length < 5 || workers.length > 35) {
       generateError("비상 근무자는 최소 5명 ~ 최대 35명 등록할 수 있습니다.");
+    }
+  }
+
+  static #validateSame(a, b) {
+    const sortedA = [...a].sort();
+    const sortedB = [...b].sort();
+
+    for (let i = 0; i < a.length; i++) {
+      if (sortedA[i] !== sortedB[i]) {
+        generateError("비상 근무자는 평일 순번, 휴일 순번에 각각 1회 편성되어야 합니다.");
+      }
     }
   }
 }
