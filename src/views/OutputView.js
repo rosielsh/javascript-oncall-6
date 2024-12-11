6b62b2be353e07b9bc3f1ecfc4b49ba96525b0a2
suhwa <tnghk9611@naver.com>
2024-12-11 17:00:00 +0900

    feat: 결과를 출력하는 메서드 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -6,7 +6,31 @@ class OutputView {
     this.#printEmptyLine();
   }
 
-  static printResult(month, startDay, assignedInfo) {}
+  static printResult(month, startDay, assignedInfo) {
+    const logs = [];
+    const startIdx = {
+      월: 0,
+      화: 1,
+      수: 2,
+      목: 3,
+      금: 4,
+      토: 5,
+      일: 6,
+    };
+
+    const DAY_OF_WEEK = ["월", "화", "수", "목", "금", "토", "일"];
+
+    let idx = startIdx[startDay];
+    for (let day = 1; day <= assignedInfo.length; day++) {
+      logs.push(
+        `${month}월 ${day}일 ${DAY_OF_WEEK[idx++ % 7]}${
+          assignedInfo[day - 1].type === "WEEKDAY_LEGAL" ? "(휴일)" : ""
+        } ${assignedInfo[day - 1].worker}`
+      );
+    }
+
+    Console.print(logs.join("\n"));
+  }
 
   static #printEmptyLine() {
     Console.print("");
