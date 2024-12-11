8aab67f3d5d6b5201b319d108facd69f87d901f8
suhwa <tnghk9611@naver.com>
2024-12-11 15:26:21 +0900

    test: 다음 근무자를 구하는 테스트 작성

---
+++ __tests__/models/Workers.test.js
@@ -0,0 +1,10 @@
+descibe("Workers 클래스 테스트 작성", () => {
+  test("다음 근무할 순번의 근무자를 알 수 있다", () => {
+    const workersInput = ["루루", "솔로스타", "수아", "슬링키", " 참새"];
+    const workers = new Workers(workersInput);
+
+    workersInput.forEach((worker) => {
+      expect(workers.getNextWorker()).toEqual(worker);
+    });
+  });
+});
